f1d9bcc5a8a95ff68abde07f3d8172129652cd82
Devind Dev <devdevind@gmail.com>
2026-03-28 21:33:18 -0700

    fix vercel build compatibility

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -4,14 +4,19 @@ set -eu
 
 ROOT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)
 DIST_DIR="$ROOT_DIR/dist"
+PUBLIC_DIR="$ROOT_DIR/public"
 
-rm -rf "$DIST_DIR"
-mkdir -p "$DIST_DIR/images"
+rm -rf "$DIST_DIR" "$PUBLIC_DIR"
 
-cp "$ROOT_DIR/site/index.html" "$DIST_DIR/index.html"
-cp "$ROOT_DIR/site/styles.css" "$DIST_DIR/styles.css"
-cp "$ROOT_DIR/site/favicon.svg" "$DIST_DIR/favicon.svg"
+for OUTPUT_DIR in "$DIST_DIR" "$PUBLIC_DIR"
+do
+    mkdir -p "$OUTPUT_DIR/images"
 
-cp "$ROOT_DIR/docs/images/lockscreen-widget-and-shortcuts.jpg" "$DIST_DIR/images/lockscreen-widget-and-shortcuts.jpg"
-cp "$ROOT_DIR/docs/images/widget-background-white.png" "$DIST_DIR/images/widget-background-white.png"
-cp "$ROOT_DIR/docs/images/widget-background-dark.png" "$DIST_DIR/images/widget-background-dark.png"
+    cp "$ROOT_DIR/site/index.html" "$OUTPUT_DIR/index.html"
+    cp "$ROOT_DIR/site/styles.css" "$OUTPUT_DIR/styles.css"
+    cp "$ROOT_DIR/site/favicon.svg" "$OUTPUT_DIR/favicon.svg"
+
+    cp "$ROOT_DIR/docs/images/lockscreen-widget-and-shortcuts.jpg" "$OUTPUT_DIR/images/lockscreen-widget-and-shortcuts.jpg"
+    cp "$ROOT_DIR/docs/images/widget-background-white.png" "$OUTPUT_DIR/images/widget-background-white.png"
+    cp "$ROOT_DIR/docs/images/widget-background-dark.png" "$OUTPUT_DIR/images/widget-background-dark.png"
+done
